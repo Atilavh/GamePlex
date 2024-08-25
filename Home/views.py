5d6849b3624e1843a4766efeae024cabd045cacd
AtilaVh <atilavalizadeh86@gmail.com>
2024-08-26 02:11:03 +0330

    Update views.py of Home_module

--- Home/views.py
+++ Home/views.py
@@ -3,10 +3,16 @@ from django.shortcuts import render
 
 # Create your views here.
 
-
+# Header-Render-Partial-->
 def header_component(request):
     return render(request, 'header_component/header_partial.html')
 
 
+# Home-Page-->
+def home_page(request):
+    return render(request, 'Base/index.html')
+
+
+# Footer-Render-Partial-->
 def footer_component(request):
     return render(request, 'footer_component/footer_partial.html')
