d741137cf90bc667ee889ee5452cbf961500a700
AtilaVh <atilavalizadeh86@gmail.com>
2024-08-27 18:14:26 +0330

    Update account_module urls

--- accounts_module/urls.py
+++ accounts_module/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('Register', views.register_page, name='Register'),
+    path('Register/', views.register_page, name='Register'),
+    path('login/', views.login_page, name='Login'),
+
+    path('activate-account/<email_code>', views.ActivateAccountView.as_view(), name='active_page')
 ]
