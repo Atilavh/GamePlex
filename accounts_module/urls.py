23a12f9d1dffb4321c581ef097b555f7c3aef1d9
AtilaVh <atilavalizadeh86@gmail.com>
2024-08-29 02:11:36 +0330

    Update account_module urls.py for logout

--- accounts_module/urls.py
+++ accounts_module/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('Register/', views.register_page, name='Register'),
     path('login/', views.login_page, name='Login'),
+    path('logout/', views.logout_page, name='Logout'),
 
     path('activate-account/<email_code>', views.ActivateAccountView.as_view(), name='active_page')
 ]
