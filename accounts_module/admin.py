85738e7d2158b60df6baa9b912f946c5840d6788
AtilaVh <atilavalizadeh86@gmail.com>
2024-08-30 14:43:18 +0330

    Update and custom admin.py

--- accounts_module/admin.py
+++ accounts_module/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from . import models
+
 
 # Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'email', 'phone_number', 'is_active', 'is_superuser', 'is_staff')
+
+
+admin.site.register(models.User, UserAdmin)
