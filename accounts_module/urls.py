4a6c04a9f9437603fdaaa45312a6a904ceb3d8f4
AtilaVh <atilavalizadeh86@gmail.com>
2024-08-30 14:42:46 +0330

    Update urls.py account_module

--- accounts_module/urls.py
+++ accounts_module/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('Register/', views.register_page, name='Register'),
     path('login/', views.login_page, name='Login'),
+    path('forgot/', views.forgot_password_page, name='Forgot'),
     path('logout/', views.logout_page, name='Logout'),
 
     path('activate-account/<email_code>', views.ActivateAccountView.as_view(), name='active_page')
