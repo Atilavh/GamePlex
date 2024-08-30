a0b194e6a8c4253cb4e499dff23a48f7fae18b9b
AtilaVh <atilavalizadeh86@gmail.com>
2024-08-30 14:45:10 +0330

    Update models.py accounts_module and Add new field

--- accounts_module/models.py
+++ accounts_module/models.py
@@ -4,14 +4,16 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
 # Create your models here.
 
-class Registration(AbstractBaseUser, PermissionsMixin):
+class User(AbstractBaseUser, PermissionsMixin):
     USERNAME_FIELD = 'username'
     full_name = models.CharField(max_length=20, verbose_name='نام کامل')
     username = models.CharField(max_length=100, verbose_name='نام کاربری', unique=True)
-    phone_number = models.CharField(max_length=100, verbose_name='شماره همراه', unique=True)
+    phone_number = models.CharField(max_length=11, verbose_name='شماره همراه', unique=True)
     email = models.EmailField(max_length=100, verbose_name='ایمیل', unique=True)
     email_active_code = models.CharField(max_length=48, verbose_name='کد فعال کننده')
-    is_active = models.BooleanField(verbose_name='فعال / غیرفعال')
+    is_active = models.BooleanField(verbose_name='وضعیت اکانت (فعال / غیر فعال)')
+    is_superuser = models.BooleanField(default=False, verbose_name='کارمند سایت')
+    is_staff = models.BooleanField(default=False, verbose_name='مدیر سایت')
 
     class Meta:
         verbose_name = 'کاربر'
