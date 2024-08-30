d23477c1c3272ccf481ef83164167d15e6c75a66
AtilaVh <atilavalizadeh86@gmail.com>
2024-08-30 14:46:42 +0330

    Drop this module

--- Competitions/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.competitions_list, name='competitions_list'),
-]
--- Competitions/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-
-# Create your views here.
-
-def competitions_list(request):
-    return render(request, 'competitions_list/competitions_list.html')
