e0efe487cce4d68732e6b4aba0881012dbcce234
AtilaVh <atilavalizadeh86@gmail.com>
2024-08-31 13:42:48 +0330

    Update views.py Tournaments

--- Tournaments/views.py
+++ Tournaments/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render, redirect
-from Tournaments.models import Tournament, Upload
+from Tournaments.models import Tournament
 from Tournaments.forms import Profile
 
 
 # Create your views here.
 
 def tournament_list(request):
-    tournaments = Tournament.objects.all()
-
+    tournaments = Tournament.objects.filter(is_active=True)
     context = {
         'tournaments': tournaments
     }
@@ -15,13 +14,13 @@ def tournament_list(request):
 
 
 def tournament_image_save(request):
-    user = Profile(request.POST, request.FILES)
-    if user.is_valid():
-        photo = Upload(image=request.FILES['profile'])
-        print(photo)
-        photo.save()
-        return redirect('/home_page/')
-
+    image_of_tournament = Profile()
+    if request.method == 'POST':
+        image_of_tournament = Profile(request.POST, request.FILES)
+        if image_of_tournament.is_valid():
+            image = Tournament(image=request.FILES["Image"])
+            image.save()
+            return redirect('upload')
     return render(request, 'competitions_list/image.html', {
-        'image': user
+        'upload': image_of_tournament
     })
