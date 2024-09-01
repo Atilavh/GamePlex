f53ce7bed5306b0e6a99a52415aa2490098166b0
AtilaVh <atilavalizadeh86@gmail.com>
2024-09-01 16:17:47 +0330

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -19,9 +19,9 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-                  path('admin/', admin.site.urls),
-                  path('', include('Home.urls')),
-                  path('', include('accounts_module.urls')),
-                  path('teams/', include('Teams.urls')),
-                  path('', include('Tournaments.urls'))
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('admin/', admin.site.urls),
+    path('', include('Home.urls')),
+    path('', include('accounts_module.urls')),
+    path('teams/', include('Teams.urls')),
+    path('', include('Tournaments.urls'))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
